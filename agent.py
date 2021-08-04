c74275def868ef3eeb95b6ae6b6e607d30acf185
Ludéric <ludericvancalck@gmail.com>
2021-08-04 23:12:20 +0200

    Added new states and agent behaviors (now locate both nest and food then do round trips between the two

--- agent.py
+++ agent.py
@@ -1,11 +1,29 @@
 from random import random, choices
-from math import sin, cos, radians, pi
+from math import sin, cos, radians, pi, atan2
 from tkinter import LAST
+from enum import Enum
+from collections import deque
+
+
+class State(Enum):
+    EXPLORING = 1
+    SEEKING_FOOD = 2
+    SEEKING_NEST = 3
+
+
+def norm(vector):
+    return (sum(x ** 2 for x in vector)) ** 0.5
+
+
+def get_orientation_from_vector(vector):
+    angle = atan2(vector[1], vector[0])
+    return (360 * angle / (2 * pi)) % 360
 
 
 class Agent:
     thetas = range(0, 360)
     weights = []
+    colors = {State.EXPLORING: "blue", State.SEEKING_FOOD: "orange", State.SEEKING_NEST: "green"}
 
     def __init__(self, robot_id, x, y, speed, radius, rdwalk_factor, environment):
         self.id = robot_id
@@ -18,39 +36,86 @@ class Agent:
         self.dx = self.speed * cos(radians(self.orientation))
         self.dy = self.speed * sin(radians(self.orientation))
         self.environment = environment
-        self.searching_food = True
+        self.state = State.EXPLORING
         self.reward = 0
         self.weights = self.crw_pdf(self.thetas)
-        goal_x, goal_y = self.environment.get_food_location()
-        self.goal_vector = [goal_x - self.x, goal_y - self.y]
+        self.trace = deque([self.x, self.y], maxlen=200)
+        self.food_location_vector = [0, 0]
+        self.nest_location_vector = [0, 0]
+        self.knows_food_location = False
+        self.knows_nest_location = False
+        self.carries_food = False
 
     def __str__(self):
         return f"Bip boop, I am bot {self.id}, located at ({self.x}, {self.y})"
 
     def step(self):
         self.move()
-        self.goal_vector[0] -= self.dx # + (random()-0.5)
-        self.goal_vector[1] -= self.dy # + (random()-0.5)
-        self.turn()
-        # self.add_orientation_noise() # odometry
-        # self.orientation += (random()-0.5)*45
-        # add random walk code
+        self.update_trace()
+        self.update_goal_vectors()
+        self.update_behavior()
+        self.update_orientation_based_on_state()
+
+    def update_trace(self):
+        self.trace.appendleft(self.y)
+        self.trace.appendleft(self.x)
+
+    def update_behavior(self):
         sensing_food = self.environment.senses_food(self)
         sensing_nest = self.environment.senses_nest(self)
-        if self.searching_food and sensing_food:
-            self.searching_food = False
-            goal_x, goal_y = self.environment.get_nest_location()
-            self.goal_vector = [goal_x - self.x, goal_y - self.y]
-        if not self.searching_food and sensing_nest:
-            self.searching_food = True
-            self.reward += 1
-            goal_x, goal_y = self.environment.get_food_location()
-            self.goal_vector = [goal_x - self.x, goal_y - self.y]
+        if sensing_food:
+            self.set_food_vector()
+            self.knows_food_location = True
+            self.carries_food = True
+        if sensing_nest:
+            self.set_nest_vector()
+            self.knows_nest_location = True
+            if self.carries_food:
+                self.reward += 1
+                self.carries_food = False
+
+        if self.state == State.EXPLORING:
+            if self.knows_food_location and sensing_nest:
+                self.state = State.SEEKING_FOOD
+            if self.knows_nest_location and sensing_food:
+                self.state = State.SEEKING_NEST
+
+        elif self.state == State.SEEKING_FOOD and sensing_food:
+            if self.knows_nest_location:
+                self.state = State.SEEKING_NEST
+            else:
+                self.state = State.EXPLORING
+
+        elif self.state == State.SEEKING_NEST and sensing_nest:
+            if self.knows_food_location:
+                self.state = State.SEEKING_FOOD
+            else:
+                self.state = State.EXPLORING
+
+    def update_orientation_based_on_state(self):
+
+        if self.state == State.EXPLORING:
+            self.turn_randomly()
+        elif self.state == State.SEEKING_FOOD:
+            self.orientation = get_orientation_from_vector(self.food_location_vector)
+        elif self.state == State.SEEKING_NEST:
+            self.orientation = get_orientation_from_vector(self.nest_location_vector)
+
+    def update_goal_vectors(self):
+        self.food_location_vector[0] -= self.dx
+        self.food_location_vector[1] -= self.dy
+        self.nest_location_vector[0] -= self.dx
+        self.nest_location_vector[1] -= self.dy
+
+    def set_food_vector(self):
+        food_x, food_y = self.environment.get_food_location()
+        self.food_location_vector = [food_x - self.x, food_y - self.y]
+
+    def set_nest_vector(self):
+        nest_x, nest_y = self.environment.get_nest_location()
+        self.nest_location_vector = [nest_x - self.x, nest_y - self.y]
 
     def move(self):
-        # self.orientation = (self.orientation + self.rdwalk_factor * (random()-0.5) * 360) % 360
-        # self.dx = (1-self.rdwalk_factor) * self.speed * cos(radians(self.orientation)) + self.rdwalk_factor * self.speed * cos(radians(random()*360))
-        # self.dy = (1-self.rdwalk_factor) * self.speed * sin(radians(self.orientation)) + self.rdwalk_factor * self.speed * sin(radians(random()*360))
         self.dx = self.speed * cos(radians(self.orientation))
         self.dy = self.speed * sin(radians(self.orientation))
         collide_x, collide_y = self.environment.check_border_collision(self, self.x + self.dx, self.y + self.dy)
@@ -61,10 +126,8 @@ class Agent:
         self.x += self.dx
         self.y += self.dy
 
-    def turn(self):
-        angle = 0
-        if self.rdwalk_factor != 1:
-            angle = choices(self.thetas, self.weights)[0]
+    def turn_randomly(self):
+        angle = choices(self.thetas, self.weights)[0]
         self.orientation = (self.orientation + angle) % 360
 
     def flip_horizontally(self):
@@ -76,22 +139,25 @@ class Agent:
         self.dy = -self.dy
 
     def draw(self, canvas):
-        if self.searching_food:
-            circle = canvas.create_oval(self.x - self.radius,
-                                        self.y - self.radius,
-                                        self.x + self.radius,
-                                        self.y + self.radius,
-                                        fill="blue")
-        else:
-            circle = canvas.create_oval(self.x - self.radius,
-                                        self.y - self.radius,
-                                        self.x + self.radius,
-                                        self.y + self.radius,
-                                        fill="red")
+        circle = canvas.create_oval(self.x - self.radius,
+                                    self.y - self.radius,
+                                    self.x + self.radius,
+                                    self.y + self.radius,
+                                    fill=self.colors[self.state])
         self.draw_goal_vector(canvas)
+        # self.draw_trace(canvas)
+
+    def draw_trace(self, canvas):
+        tail = canvas.create_line(*self.trace)
 
     def draw_goal_vector(self, canvas):
-        arrow = canvas.create_line(self.x, self.y, self.x + self.goal_vector[0], self.y + self.goal_vector[1], arrow=LAST)
+        goal_vector = [0, 0]
+        if self.state == State.SEEKING_FOOD:
+            goal_vector = self.food_location_vector
+        elif self.state == State.SEEKING_NEST:
+            goal_vector = self.nest_location_vector
+        arrow = canvas.create_line(self.x, self.y, self.x + goal_vector[0], self.y + goal_vector[1],
+                                   arrow=LAST)
 
     def crw_pdf(self, thetas):
         res = []
@@ -100,7 +166,6 @@ class Agent:
             denom = 2 * pi * (1 + self.rdwalk_factor ** 2 - 2 * self.rdwalk_factor * cos(radians(t)))
             f = 1
             if denom != 0:
-                f = num/denom
+                f = num / denom
             res.append(f)
-        print(res)
         return res
