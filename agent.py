9e0bddf05be6d52b834f959f2d7db5b70b37e0fc
Ludéric <ludericvancalck@gmail.com>
2021-08-17 13:17:24 +0200

    Robots can now get lost (TODO add noise parameters to config file and sample random noise for each robot)

--- agent.py
+++ agent.py
@@ -21,6 +21,12 @@ def get_orientation_from_vector(vector):
     return (360 * angle / (2 * pi)) % 360
 
 
+def rotation_matrix(angle):
+    theta = radians(angle)
+    return np.array(((np.cos(theta), -np.sin(theta)),
+                     (np.sin(theta), np.cos(theta))))
+
+
 class Agent:
     thetas = np.arange(0, 360)
     max_levi_steps = 1000
@@ -37,16 +43,20 @@ class Agent:
         self.orientation = random() * 360  # 360 degree angle
         self.displacement = np.array([0, 0]).astype('float64')
         self.environment = environment
-        self.state = State.EXPLORING
+        self.state = State.SEEKING_FOOD
         self.reward = 0
         self.crw_weights = self.crw_pdf(self.thetas)
         self.levi_weights = self.levi_pdf(self.max_levi_steps)
         self.levi_counter = 1
-        self.trace = deque(self.pos, maxlen=200)
-        self.food_location_vector = np.array([0, 0]).astype('float64')
-        self.nest_location_vector = np.array([0, 0]).astype('float64')
-        self.knows_food_location = False
-        self.knows_nest_location = False
+        self.trace = deque(self.pos, maxlen=100)
+        # self.food_location_vector = np.array([0, 0]).astype('float64')
+        # self.nest_location_vector = np.array([0, 0]).astype('float64')
+        # self.knows_food_location = False
+        # self.knows_nest_location = False
+        self.food_location_vector = self.environment.get_food_location() - self.pos
+        self.nest_location_vector = self.environment.get_nest_location() - self.pos
+        self.knows_food_location = True
+        self.knows_nest_location = True
         self.carries_food = False
 
     def __str__(self):
@@ -62,6 +72,18 @@ class Agent:
     def update_trace(self):
         self.trace.appendleft(self.pos[1])
         self.trace.appendleft(self.pos[0])
+
+    def is_stationary(self):
+        trace_length = len(self.trace) // 2
+        tolerance = 3
+        if trace_length < 2 * tolerance:  # If robot hasn't done 2*tolerance steps yet, consider it is moving
+            return False
+        trace_x = [self.trace[2*i] for i in range(trace_length)]
+        trace_y = [self.trace[2*i+1] for i in range(trace_length)]
+        max_x_gap = max(trace_x) - min(trace_x)
+        max_y_gap = max(trace_y) - min(trace_y)
+
+        return max_x_gap < tolerance * self.speed and max_y_gap < tolerance * self.speed
 
     def update_behavior(self):
         sensing_food = self.environment.senses_food(self)
@@ -95,16 +117,23 @@ class Agent:
             else:
                 self.state = State.EXPLORING
 
-    def update_orientation_based_on_state(self):
+        if self.is_stationary():
+            if self.state == State.SEEKING_NEST:
+                self.knows_nest_location = False
+                self.state = State.EXPLORING
+            elif self.state == State.SEEKING_FOOD:
+                self.knows_food_location = False
+                self.state = State.EXPLORING
 
+    def update_orientation_based_on_state(self):
+        turn_angle = 0
         if self.state == State.EXPLORING:
-            if self.levi_counter <= 1:
-                self.turn_randomly()
-            self.update_levi_counter()
+            turn_angle = self.get_levi_turn_angle()
         elif self.state == State.SEEKING_FOOD:
-            self.orientation = get_orientation_from_vector(self.food_location_vector)
+            turn_angle = get_orientation_from_vector(self.food_location_vector) - self.orientation
         elif self.state == State.SEEKING_NEST:
-            self.orientation = get_orientation_from_vector(self.nest_location_vector)
+            turn_angle = get_orientation_from_vector(self.nest_location_vector) - self.orientation
+        self.turn(turn_angle)
 
     def update_goal_vectors(self):
         self.food_location_vector -= self.displacement
@@ -117,25 +146,56 @@ class Agent:
         self.nest_location_vector = self.environment.get_nest_location() - self.pos
 
     def move(self):
+        # Robot's will : calculates where it wants to end up and check if there are no border walls
         self.displacement = self.speed * np.array([cos(radians(self.orientation)), sin(radians(self.orientation))])
         collide_x, collide_y = self.environment.check_border_collision(self,
                                                                        self.pos[0] + self.displacement[0],
                                                                        self.pos[1] + self.displacement[1])
+        # If border in front, flip orientation along wall axis
         if collide_x:
             self.flip_horizontally()
         if collide_y:
             self.flip_vertically()
 
-        self.pos += self.displacement
+        # Real movement, subject to noise, clamped to borders
+        n_hor = 0.25 * self.speed * (random() - 0.3)
+        n_vert = -0.25 * self.speed * (random() - 0.5)
+        noise_rel = np.array([n_vert, n_hor])  # noise vector in robot's relative coordinates
+        noise = rotation_matrix(self.orientation).dot(noise_rel)  # noise vector in absolute (x, y) coordinates
+        self.pos = self.clamp_to_map(self.pos + self.displacement + noise)
+
+    def turn(self, angle):
+        noise_angle = 2 * (random() - 0.5)
+        self.rotate_vectors(noise_angle)
+        self.orientation = (self.orientation + angle) % 360
+
+    def rotate_vectors(self, angle):
+        rot_mat = rotation_matrix(angle)
+        self.food_location_vector = rot_mat.dot(self.food_location_vector)
+        self.nest_location_vector = rot_mat.dot(self.nest_location_vector)
+
+    def clamp_to_map(self, new_position):
+        if new_position[0] < self.radius:
+            new_position[0] = self.radius
+        if new_position[1] < self.radius:
+            new_position[1] = self.radius
+        if new_position[0] > self.environment.width - self.radius:
+            new_position[0] = self.environment.width - self.radius
+        if new_position[1] > self.environment.height - self.radius:
+            new_position[1] = self.environment.height - self.radius
+        return new_position
 
     def update_levi_counter(self):
         self.levi_counter -= 1
         if self.levi_counter <= 0:
             self.levi_counter = choices(range(1, self.max_levi_steps + 1), self.levi_weights)[0]
 
-    def turn_randomly(self):
-        angle = choices(self.thetas, self.crw_weights)[0]
-        self.orientation = (self.orientation + angle) % 360
+    def get_levi_turn_angle(self):
+        angle = 0
+        if self.levi_counter <= 1:
+            angle = choices(self.thetas, self.crw_weights)[0]
+        self.update_levi_counter()
+        return angle
 
     def flip_horizontally(self):
         self.orientation = (180 - self.orientation) % 360
@@ -151,7 +211,7 @@ class Agent:
                                     self.pos[0] + self.radius,
                                     self.pos[1] + self.radius,
                                     fill=self.colors[self.state])
-        self.draw_goal_vector(canvas)
+        #self.draw_goal_vector(canvas)
         self.draw_trace(canvas)
 
     def draw_trace(self, canvas):
