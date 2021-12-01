1151e3c3f45dd17cdd4643442ce8129957577569
lvcalck <ludericvancalck@gmail.com>
2021-12-01 14:54:22 +0100

    data analysis

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,3 +1,5 @@
+import re
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,19 +28,66 @@ def main():
     pd.DataFrame(w_dec_lin).boxplot(ax=axs[3]).set_title("W-Decaying Linear")
     pd.DataFrame(dec_exp).boxplot(ax=axs[4]).set_title("Decaying Exp")
     pd.DataFrame(w_dec_exp).boxplot(ax=axs[5]).set_title("W-Decaying Exp")
-    #pd.DataFrame(dec_exp_const).boxplot(ax=axs[5]).set_title("Dec Exp Const")
+    # pd.DataFrame(dec_exp_const).boxplot(ax=axs[5]).set_title("Dec Exp Const")
     plt.show()
 
 
 def main2():
-    df = pd.read_csv("../data/old_data/data_sorted.txt", header=None)
+    df = pd.read_csv("../data/behaviors/rewards/25honest.txt", header=None)
     plt.plot(df.apply(np.mean, axis=0))
     plt.show()
 
 
 def compare_behaviors():
-    pass
+    filenames = ["25honest", "24honest_1saboteur", "24careful_s3_1saboteur"]
+    items_collected_plot(filenames)
+    rewards_plot(filenames)
+    honest_vs_careful()
+
+
+def honest_vs_careful():
+    h_name = "24honest_1saboteur"
+    c_name = "24careful_s3_1saboteur"
+    hdf = pd.read_csv(f"../data/behaviors/rewards/{h_name}.txt", header=None)
+    cdf = pd.read_csv(f"../data/behaviors/rewards/{c_name}.txt", header=None)
+    hdiff = hdf.iloc[:, :24].apply(np.mean, axis=1) - hdf.iloc[:, 24]
+    cdiff = cdf.iloc[:, :24].apply(np.mean, axis=1) - cdf.iloc[:, 24]
+    fig, axs = plt.subplots()
+    axs.set_title("Reward Difference with Saboteur")
+    hist(hdiff.values.flatten(), 1.5)
+    hist(cdiff.values.flatten(), 1.5)
+    axs.legend(["honest", "careful"])
+    plt.show()
+
+
+def items_collected_plot(filenames):
+    fig, axs = plt.subplots()
+    for filename in filenames:
+        collected = pd.read_csv(f"../data/behaviors/items_collected/{filename}.txt", header=None)
+        n_honest = int(re.search('[0-9]+', filename).group())
+        hist(collected.iloc[:, :n_honest].values.flatten(), 1)
+    axs.legend(filenames)
+    axs.set_title("Items Collected Distribution")
+    plt.show()
+
+
+def rewards_plot(filenames):
+    fig, axs = plt.subplots()
+    for filename in filenames:
+        rewards = pd.read_csv(f"../data/behaviors/rewards/{filename}.txt", header=None)
+        n_honest = int(re.search('[0-9]+', filename).group())
+        hist(rewards.iloc[:, :n_honest].values.flatten(), 1)
+    axs.legend(filenames)
+    axs.set_title("Honest Rewards Distribution")
+    plt.show()
+
+
+def hist(values, precision):
+    bins = np.arange(np.floor(np.min(values)), np.ceil(np.max(values)), precision)
+    n, bins = np.histogram(values, bins=bins, density=True)
+    plt.plot(bins[:-1], n)
 
 
 if __name__ == '__main__':
-    main2()
+    # main2()
+    compare_behaviors()
